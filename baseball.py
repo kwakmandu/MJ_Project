ff5bb44eee403a8b8ef357ba2ee03e6b4031aade
mandu <wbslalswns@naver.com>
2024-07-10 14:53:20 +0900

    raise all illegal input

--- baseball.py
+++ baseball.py
@@ -1,7 +1,22 @@
 class Baseball:
+    LENGTH_OF_ANSWER = 3
+
     def __init__(self):
         self.answer = 123
 
-    def guess(self, user_try):
+    def assert_if_illegal_input(self, user_try):
         if not user_try:
             raise TypeError()
+
+        if len(str(user_try)) != self.LENGTH_OF_ANSWER:
+            raise TypeError()
+
+        for num in str(user_try):
+            if not ord("0") <= ord(num) <= ord("9"):
+                raise TypeError()
+
+        if len(set(list(str(user_try)))) != self.LENGTH_OF_ANSWER:
+            raise TypeError()
+
+    def guess(self, user_try):
+        self.assert_if_illegal_input(user_try)
--- test_baseball.py
+++ test_baseball.py
@@ -7,8 +7,16 @@ class TestBaseball(TestCase):
     def setUp(self):
         self.game = Baseball()
 
-    def test_no_input(self):
-        with self.assertRaises(TypeError):
-            self.game.guess(None)
+    def assert_if_illegal_argument(self, user_try):
+        try:
+            self.game.guess(user_try)
+            self.fail()
+        except TypeError:
+            pass
 
+    def test_illegal_input(self):
+        test_cases = ("None", "12", 12, "1004", 0, "12s", "121")
+        for test_case in test_cases:
+            with self.subTest(f"{test_case}"):
+                self.assert_if_illegal_argument(test_case)
 
